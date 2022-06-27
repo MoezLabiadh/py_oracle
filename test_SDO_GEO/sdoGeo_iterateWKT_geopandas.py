3869234c7c5107dc78ee12bbc8d2047d1f258011
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-06-27 16:13:08 -0700

    Update sdoGeo_iterateWKT_geopandas.py

--- test_SDO_GEO/sdoGeo_iterateWKT_geopandas.py
+++ test_SDO_GEO/sdoGeo_iterateWKT_geopandas.py
@@ -1,16 +1,31 @@
 """
-This Script runs Spatial Oracle Queries against external shapes.
- 
-Main steps:
+This Script runs SQL Queries against BCGW Spatial tables/views
+and external shapes (AOI).
+
+User Inputs:
+    1. AOI - ESRI shp and featureclass/gdb are supported.
+    2. bcgw_user - your BCGW/idwprod1 username
+    3. bcgw_pwd - your BCGW/idwprod1 password
+    4. SQL - query to execute.
+
+Steps:
     1. Connect to Database (BCGW).
     2. Convert ESRI format shape to Geopandas format. 
     3. Retireve Geometry WKT string and Spatial ref. for each feature.
     4. Run the SQL Query.
     5. Export Query Results to an Excel file.
+
+Dependencies:
+    1. cx_Oracle
+    2. Geopandas
+    3. Pandas
     
-The query used in this recipe is looking for Active Crown Tenures
-intersecting with an AOI - SDO_RELATE (ANYINTERACT). Customize the "sql" parameter 
-for other types of Queries.
+The query used in this recipe is looking for Active Aqua Crown Tenures
+intersecting with an AOI (SPATIAL JOIN). Customize the "sql" parameter 
+for other  Query types.
+
+See Oracle doscumentation for full list of Spatial Operators:
+https://docs.oracle.com/cd/E11882_01/appdev.112/e11830/sdo_operat.htm#SPATL110
 
 """
 
@@ -32,21 +47,21 @@ def connect_to_DB (username,password,hostname):
     return connection
 
 
-def esri_to_gdf (inp):
+def esri_to_gdf (aoi):
     """Returns a Geopandas file (gdf) based on 
        an ESRI format vector (shp or featureclass/gdb)"""
     
-    if '.shp' in inp: 
-        gdf = gpd.read_file(inp)
+    if '.shp' in aoi: 
+        gdf = gpd.read_file(aoi)
     
-    elif '.gdb' in inp:
-        l = inp.split ('.gdb')
+    elif '.gdb' in aoi:
+        l = aoi.split ('.gdb')
         gdb = l[0] + '.gdb'
-        fc = os.path.basename(inp)
+        fc = os.path.basename(aoi)
         gdf = gpd.read_file(filename= gdb, layer= fc)
         
     else:
-        raise Exception ('Format not recognized. Please provide an shp or featureclass (gdb)')
+        raise Exception ('Format not recognized. Please provide and shp or feature class (gdb)')
     
     return gdf
     
@@ -58,7 +73,7 @@ def get_wkt_srid (gdf):
     
     srid = gdf.crs.to_epsg()
     if srid != 3005:
-        raise Exception ('Shape must be in BC Albers Projection.')
+        raise Exception ('Shape should be in BC Albers Projection.')
     
     # Generate WKT strings. 
     #If WKT string is larger then 4000 characters (ORACLE VARCHAR limit), 
@@ -72,13 +87,14 @@ def get_wkt_srid (gdf):
         if len (wkt) < 4000:
             print ('FULL WKT returned for {} - within Oracle VARCHAR limit'.format (f)) 
             wkt_dict [f] = wkt
-    
+            
         else:
             print ('Geometry will be Simplified for {} - beyond Oracle VARCHAR limit'.format (f))
             for s in range (10, 10000, 10):
                 wkt_sim = row['geometry'].simplify(s).wkt
                 if len(wkt_sim) < 4000:
-                    break                
+                    break
+                
             print ('Geometry Simplified with Tolerance {} m'.format (s))            
             wkt_dict [f] = wkt_sim 
                 
@@ -132,7 +148,7 @@ def generate_report (workspace, df_list, sheet_list, filename):
 
 
 def main ():
-    inp = r"\\spatialfiles.bcgov\Work\lwbc\visr\Workarea\moez_labiadh\DATASETS\local_data.gdb\Admin\Aqua_finFish_Broughton_zone"
+    aoi= r"\\spatialfiles.bcgov\Work\...\local_data.gdb\Admin\Aqua_finFish_Broughton_zone"
     
     hostname = 'bcgw.bcgov/idwprod1.bcgov'
     bcgw_user = os.getenv('bcgw_user')
@@ -142,7 +158,7 @@ def main ():
     connection = connect_to_DB (bcgw_user,bcgw_pwd,hostname)
     
     print ('\nReading the input file...')
-    gdf = esri_to_gdf (inp)
+    gdf = esri_to_gdf (aoi)
     
     print ('\nGetting WKT and SRID...')
     wkt_dict, srid = get_wkt_srid (gdf)
@@ -151,7 +167,7 @@ def main ():
     sql =  """
             SELECT*
             FROM WHSE_TANTALIS.TA_CROWN_TENURES_SVW t
-            WHERE t.TENURE_SUBPURPOSE = 'FIN FISH'
+            WHERE t.TENURE_PURPOSE = 'AQUACULTURE'
             AND t.TENURE_STAGE = 'TENURE'
             AND SDO_RELATE (t.SHAPE, SDO_GEOMETRY('{w}', {s}),
                             'mask=ANYINTERACT') = 'TRUE'
@@ -164,13 +180,12 @@ def main ():
         query = sql.format(w = v,  s = srid)
         df = read_query(connection,query)
         dfs.append(df)
-        keys.append(k)
-    
-    sheets = ['Intersect Feature ' + k for k in keys]
+        keys.append(k)  
+    sheets = ['Intersect ' + k for k in keys]
     
     print ('\nExporting Query Results...')
-    wks = r'\\spatialfiles.bcgov\Work\...\WKT_geoPandas'
-    generate_report (wks, dfs, sheets, 'Query_Results')
+    out_loc = r'\\spatialfiles.bcgov\Work\...\outputs'
+    generate_report (out_loc, dfs, sheets, 'Query_Results')
 
 
 main ()
