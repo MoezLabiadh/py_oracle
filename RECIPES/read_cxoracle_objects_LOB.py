db11ddec68784a873932e53a22d5bc8537bf1d17
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2022-08-10 10:44:29 -0700

    Update read_cxoracle_objects_LOB.py

--- RECIPES/read_cxoracle_objects_LOB.py
+++ RECIPES/read_cxoracle_objects_LOB.py
@@ -17,3 +17,24 @@ def ObjectRepr(obj):
             returnValue[attr.name] = value
       
     return returnValue
+
+def sdoOrdinates_2_wkt (lob_dic):
+    """Converts cxoracle LBO dict to wkt string - only polygons for now"""
+    sdo_gtyp = lob_dic['SDO_GTYPE']
+    
+    if str(sdo_gtyp)[-2:] == '03':
+        sdo_ord = lob_dic['SDO_ORDINATES']
+        l_len = len(sdo_ord)
+        l_paired = [str(sdo_ord[i]) + " " + str(sdo_ord[i+1]) for i in range(0, l_len-1, 2)]
+        
+        if l_len % 2 == 1:
+            l_paired.append(sdo_ord[l_len-1])
+        
+        s_ord = ", ".join (x for x in l_paired)
+        
+        s_wkt = "POLYGON (({}))".format(s_ord) 
+        
+    else:
+        print('This Geometry is not a Polygon.')
+    
+    return s_wkt
