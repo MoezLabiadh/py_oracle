30fc4d845b1d50166e76a1152bc6a9277bc64857
moez-lab <60794110+MoezLabiadh@users.noreply.github.com>
2024-02-08 14:50:02 -0800

    Update pyodbc_connection.py

--- RECIPES/pyodbc_connection.py
+++ RECIPES/pyodbc_connection.py
@@ -1,53 +1,88 @@
-
-import os
+import json
 import pyodbc
 import pandas as pd
-from tantalis_bigQuery import load_sql
 
+
+def get_db_cnxinfo (dbname='BCGW'):
+    """ Retrieves db connection params from the config file"""
+    
+    with open(r'H:\config\db_config.json', 'r') as file:
+        data = json.load(file)
+        
+    if dbname in data:
+        cnxinfo = data[dbname]
+
+        return cnxinfo
+    
+    raise KeyError(f"Database '{dbname}' not found.")
+    
+    
+    
 # USE THIS WITHIN DESKTOP
 def connect_to_DB (driver,server,port,dbq, username,password):
     """ Returns a connection to Oracle database"""
     try:
-        connectString ="""
+        connectString =f"""
                     DRIVER={driver};
                     SERVER={server}:{port};
                     DBQ={dbq};
-                    Uid={uid};
-                    Pwd={pwd}
-                       """.format(driver=driver,server=server, port=port,
-                                  dbq=dbq,uid=username,pwd=password)
-
+                    Uid={username};
+                    Pwd={password}
+                       """
         connection = pyodbc.connect(connectString)
+        cursor= connection.cursor()
         print  ("...Successffuly connected to the database")
     except:
         raise Exception('...Connection failed! Please check your connection parameters')
 
-    return connection
+    return connection, cursor
 
 
-def read_query(connection,query):
-    "Returns a df containing SQL Query results"
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        cols = [x[0] for x in cursor.description]
-        rows = cursor.fetchall()
-        return pd.DataFrame.from_records(rows, columns=cols)
-    
-    finally:
-        if cursor is not None:
-            cursor.close()
 
             
-   # USE THIS OUTSIDE OF DESKTOP
-connection_string ='''Driver={driver};
-            DBQ={hostname}:{sid};
-            UID={username};
-            PWD={password};
-            Authentication=LDAP;
-            Trusted_Connection="yes"
-                 '''.format(driver = '{Oracle in OraClient12Home1}', 
-                            hostname = 'bcgw.bcgov/idwprod1.bcgov', 
-                            sid = 'IDWPROD1',
-                            username = 'XXX',
-                            password = 'XXX')
+# USE THIS OUTSIDE OF DESKTOP
+def connect_to_DB (driver,hostname,sid,username,password):
+    """ Returns a connection to Oracle database"""
+    try:
+        connection_string =f"""
+                    Driver={driver};
+                    DBQ={hostname}:{sid};
+                    UID={username};
+                    PWD={password};
+                    Authentication=LDAP;
+                         """
+        connection = pyodbc.connect(connection_string)
+        cursor= connection.cursor()
+        print  ("...Successffuly connected to the database")
+    except:
+        raise Exception('...Connection failed! Please check your connection parameters')
+
+    return connection, cursor
+
+
+
+def read_query(connection, cursor, query, bvars):
+    "Returns a df containing SQL Query results"
+    try:
+        cursor.execute(query, bvars)
+        columns = [x[0] for x in cursor.description]
+        rows = cursor.fetchall()
+        df = pd.DataFrame.from_records(rows, columns=[x for x in columns])
+        
+        return df
+    
+    finally:
+        cursor.close()
+        connection.close() 
+        
+
+
+driver= [x for x in pyodbc.drivers() if x.startswith('Oracle')][0]  
+cnxinfo= get_db_cnxinfo(dbname='BCGW')
+server = cnxinfo['server']
+port= cnxinfo['port']
+dbq= cnxinfo['dbq']
+username= cnxinfo['username']
+password= cnxinfo['password']
+
+connection, cursor= connect_to_DB (driver,server,port,dbq,username,password)
